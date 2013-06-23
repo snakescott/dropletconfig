8010fff4019ba6c5074d705f457022caf5f12470
snakescott <jacob.scott@gmail.com>
2013-06-22 22:13:13 -0600

    Update config_system.bash

--- config_system.bash
+++ config_system.bash
@@ -1,4 +1,5 @@
 # requires a terminal, so scp it to remote host and then execute
+# Installs some basic packages (including ubuntu firewall and unattended-upgrades for getting security patches). 
 
 set -o errexit -o nounset
 sudo apt-get -y install git make gcc g++ screen unattended-upgrades landscape-common update-motd update-notifier ufw
